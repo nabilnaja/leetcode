613f59f2d2f92adcc25814c9d23234119b458183
nabilnaja <nabilnaja9@gmail.com>
2022-01-28 19:21:14 +0100

    Update leetcode 1 solution

--- src/leetcode1/Solution.java
+++ src/leetcode1/Solution.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> index = new HashMap<>();
+        Map<Integer, Integer> index = new HashMap<>(nums.length);
         for (int i = 0; i < nums.length; i++){
             if (index.containsKey(nums[i]))
                 return new int [] {index.get(nums[i]), i};
