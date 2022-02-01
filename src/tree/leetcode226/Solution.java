8786c2e87061c09ec69aae61426250ab11067bf0
nabilnaja <nabilnaja9@gmail.com>
2022-02-01 19:00:22 +0100

    Add leetcode 226 solution

--- src/tree/leetcode226/Solution.java
+++ src/tree/leetcode226/Solution.java
@@ -3,10 +3,13 @@ package tree.leetcode226;
 import datastructure.TreeNode;
 
 public class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
-        if (p.val != q.val) return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
     }
 }
