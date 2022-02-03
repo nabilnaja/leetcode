f601f111c74b09a7777013a8a8cb0814e4f055a5
nabilnaja <nabilnaja9@gmail.com>
2022-02-03 23:49:30 +0100

    Add leetcode 143 solution

---
+++ src/linkedList/leetcode143/Solution.java
@@ -0,0 +1,29 @@
+package linkedList.leetcode143;
+
+import datastructure.ListNode;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public void reorderList(ListNode head) {
+        ListNode node = head;
+        Stack<ListNode> stack = new Stack<>();
+        while (node != null) {
+            stack.add(node);
+            node = node.next;
+        }
+        while (!stack.isEmpty()) {
+            ListNode end = stack.pop();
+            if(end == head || head.next == end){
+                end.next = null;
+                return;
+            }
+            ListNode temp = head.next;
+            temp.next = head.next.next;
+            head.next = end;
+            end.next = temp;
+            head = head.next.next;
+        }
+    }
+}
