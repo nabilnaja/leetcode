e415876a4e3ec8828d155756f9778b1a601b8f62
nabilnaja <nabilnaja9@gmail.com>
2022-02-19 19:57:39 +0100

    add leetcode 206 solution

---
+++ src/linkedList/leetcode206/Solution.java
@@ -0,0 +1,18 @@
+package linkedList.leetcode206;
+
+import datastructure.ListNode;
+
+public class Solution {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
