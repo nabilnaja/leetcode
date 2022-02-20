ca4d6b622853dbc27e7b1bb4e945fe8331ad828c
nabilnaja <nabilnaja9@gmail.com>
2022-02-20 19:04:20 +0100

    add leetcode 105 solution

---
+++ src/tree/leetcode105/Solution.java
@@ -0,0 +1,52 @@
+package tree.leetcode105;
+
+import datastructure.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    HashMap<Integer,Integer> cache;
+    int index;
+    int cacheIndex;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        index = 0;
+        cacheIndex = 0;
+        cache = new HashMap<>();
+        int index = 0;
+        return buildTree(inorder, preorder, 0, inorder.length - 1);
+
+    }
+
+    private TreeNode buildTree(int[] inorder, int[] preorder, int inStrt, int inEnd){
+        if (inStrt > inEnd)
+            return null;
+
+        TreeNode tNode = new TreeNode(preorder[index++]);
+
+        if (inStrt == inEnd)
+            return tNode;
+
+        int inIndex  = getPos(inorder, tNode.val);
+
+        tNode.left = buildTree(inorder, preorder, inStrt, inIndex  - 1);
+        tNode.right = buildTree(inorder, preorder, inIndex  + 1, inEnd);
+
+        return tNode;
+    }
+
+    int getPos(int[] inorder, int value)
+    {
+        if (cache.containsKey(value))
+            return cache.get(value);
+        while (cacheIndex < inorder.length){
+            cache.put(inorder[cacheIndex], cacheIndex);
+            if (inorder[cacheIndex] == value){
+                return cacheIndex++;
+            }
+            cacheIndex++;
+        }
+        return -1;
+    }
+}
