f7d94b8118fda030a9348e7d3bdc1a364e74d921
nabilnaja <nabilnaja9@gmail.com>
2022-03-10 22:42:24 +0100

    start leetcode 3 solution

---
+++ src/string/leetcode3/Solution.java
@@ -0,0 +1,31 @@
+package string.leetcode3;
+
+import java.util.HashSet;
+
+public class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int start = 0;
+        int end = 0;
+        HashSet<Character> chars = new HashSet<>();
+
+        while (end < s.length()) {
+            char c = s.charAt(end);
+            if (!chars.add(c)) {
+                while (c != s.charAt(start)) {
+                    chars.remove(s.charAt(start));
+                    start++;
+                }
+                start++;
+            }
+            max = Math.max(max, end - start + 1);
+            end++;
+        }
+        return max;
+
+    }
+
+}
+
+
