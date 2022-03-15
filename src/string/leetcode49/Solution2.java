3179b99e617dcad1f057dea468a50128215e511b
nabilnaja <nabilnaja9@gmail.com>
2022-03-15 20:56:50 +0100

    add leetcode 49 solution 2

--- src/string/leetcode49/Solution2.java
+++ src/string/leetcode49/Solution2.java
@@ -2,32 +2,25 @@ package string.leetcode49;
 
 import java.util.*;
 
-public class Solution {
+public class Solution2 {
 
     public static final char CHAR = 'A';
 
     public List<List<String>> groupAnagrams(String[] strs) {
         if (strs.length == 0) return new ArrayList<>();
-        Map<String, List<String>> ans = new HashMap<>();
+        Map<Integer, List<String>> ans = new HashMap<>();
         for(String str: strs){
-            String hash = hash(str);
+            int hash = hash2(str);
             ans.putIfAbsent(hash, new ArrayList<>());
             ans.get(hash).add(str);
         }
         return new ArrayList<>(ans.values());
     }
-    private String hash(String str){
+    private int hash2(String str){
         int[] count = new int[26];
         for (char c : str.toCharArray())
             count[c - CHAR]++;
-        StringBuilder sb = new StringBuilder();
-        for (char i = 0; i < 26; i++) {
-            if(count[i] != 0){
-                sb.append(i);
-                sb.append(count[i]);
-            }
-        }
-        return sb.toString();
+        return Arrays.hashCode(count);
     }
 }
 
