00b67940f741729482ac17a8b60228b16b8986e3
nabilnaja <nabilnaja9@gmail.com>
2022-03-19 22:42:39 +0100

    Add Graph data structure

---
+++ src/datastructure/GraphNode.java
@@ -0,0 +1,23 @@
+package datastructure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphNode {
+
+    public int val;
+    public List<GraphNode> neighbors;
+
+    public GraphNode() {
+        val = 0;
+        neighbors = new ArrayList<>();
+    }
+    public GraphNode(int val) {
+        this.val = val;
+        this.neighbors = new ArrayList<>();
+    }
+    public GraphNode(int val, List<GraphNode> neighbors) {
+        this.val = val;
+        this.neighbors = neighbors;
+    }
+}
