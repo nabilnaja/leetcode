d236d75c2337b368728bf6623da8f4815458be4e
nabilnaja <nabilnaja9@gmail.com>
2022-03-19 23:00:19 +0100

    add leetcode 133 solution

---
+++ src/graph/leetcode133/Solution.java
@@ -0,0 +1,34 @@
+package graph.leetcode133;
+
+
+import datastructure.GraphNode;
+
+import java.util.*;
+
+public class Solution {
+
+    public GraphNode cloneGraph(GraphNode node) {
+        if (node == null) {
+            return null;
+        }
+
+        Queue<GraphNode> q = new LinkedList<>();
+        Map<GraphNode, GraphNode> visited = new HashMap<>();
+        q.add(node);
+        visited.put(node, new GraphNode(node.val, new ArrayList<>()));
+
+        while(!q.isEmpty()){
+            GraphNode n = q.poll();
+            for (GraphNode neighbor: n.neighbors) {
+                if (!visited.containsKey(neighbor)) {
+                    visited.put(neighbor, new GraphNode(neighbor.val, new ArrayList<>()));
+                    q.add(neighbor);
+                }
+                visited.get(n).neighbors.add(visited.get(neighbor));
+            }
+        }
+        return visited.get(node);
+    }
+}
+
+
