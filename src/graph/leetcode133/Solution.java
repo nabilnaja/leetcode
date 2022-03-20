4b66c415253fca6f1d5afd8bebaaf21f393d0a5e
nabilnaja <nabilnaja9@gmail.com>
2022-03-20 12:54:39 +0100

    add leetcode 133 solution iterative DFS

--- src/graph/leetcode133/Solution.java
+++ src/graph/leetcode133/Solution.java
@@ -7,9 +7,29 @@ import java.util.*;
 
 public class Solution {
 
-    // Todo add DFS solution
+    public GraphNode cloneGraphBFS(GraphNode node) {
+        if (node == null) {
+            return null;
+        }
 
-    public GraphNode cloneGraph(GraphNode node) {
+        Queue<GraphNode> q = new LinkedList<>();
+        Map<GraphNode, GraphNode> visited = new HashMap<>();
+        q.add(node);
+        visited.put(node, new GraphNode(node.val, new ArrayList<>()));
+
+        while(!q.isEmpty()){
+            GraphNode n = q.poll();
+            for (GraphNode neighbor: n.neighbors) {
+                if (!visited.containsKey(neighbor)) {
+                    visited.put(neighbor, new GraphNode(neighbor.val, new ArrayList<>()));
+                    q.add(neighbor);
+                }
+                visited.get(n).neighbors.add(visited.get(neighbor));
+            }
+        }
+        return visited.get(node);
+    }
+    public GraphNode cloneGraphDFS(GraphNode node) {
         if (node == null) {
             return null;
         }
