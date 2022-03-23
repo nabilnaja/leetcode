8785a6ba3d23dc5e84675722d803dfb28fd80631
nabilnaja <nabilnaja9@gmail.com>
2022-03-23 18:53:05 +0100

    add leetcode 206 recursive solution

--- src/linkedList/leetcode206/Solution.java
+++ src/linkedList/leetcode206/Solution.java
@@ -15,4 +15,14 @@ public class Solution {
         }
         return prev;
     }
+
+    public ListNode reverseListRecursion(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode tail = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return tail;
+    }
 }
