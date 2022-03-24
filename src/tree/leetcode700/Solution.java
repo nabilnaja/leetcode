1b7e3d7b4ae7f4530f43ae4a7f66066682bc51c6
nabilnaja <nabilnaja9@gmail.com>
2022-03-24 21:54:45 +0100

    add leetcode 700 solution

---
+++ src/tree/leetcode700/Solution.java
@@ -0,0 +1,11 @@
+package tree.leetcode700;
+
+import datastructure.TreeNode;
+
+public class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if(root.val == val) return root;
+        return root.val > val ? searchBST(root.left, val) : searchBST(root.right, val);
+    }
+}
