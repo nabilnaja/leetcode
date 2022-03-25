4dc8b9645fd47f79231c083edabca045f9aac864
nabilnaja <nabilnaja9@gmail.com>
2022-03-25 21:09:22 +0100

    add leetcode 235 solution

--- src/tree/leetcode235/Solution.java
+++ src/tree/leetcode235/Solution.java
@@ -3,15 +3,20 @@ package tree.leetcode235;
 import datastructure.TreeNode;
 
 public class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode n1, TreeNode n2) {
-        while (root != null)
-        {
-            if (root.val > n1.val && root.val > n2.val)
-                root = root.left;
-            else if (root.val < n1.val && root.val < n2.val)
-                root = root.right;
-            else return  root;
-        }
-        return null;
+    TreeNode result = null;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        recurseTreeHelper(root, p, q);
+        return result;
+    }
+    private boolean recurseTreeHelper(TreeNode currentNode, TreeNode p, TreeNode q) {
+        if(currentNode == null)
+            return false;
+        int l = recurseTreeHelper(currentNode.left, p, q) ? 1 : 0;
+        int r = recurseTreeHelper(currentNode.right, p, q) ? 1 : 0;
+        int mid = currentNode == p || currentNode == q ? 1 : 0;
+        int total = l + r + mid;
+        if (total >= 2)
+            result = currentNode;
+        return total == 1;
     }
 }
