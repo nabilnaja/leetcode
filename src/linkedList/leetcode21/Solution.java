a4e945112f21361ce3ea704eca4b0124507dd9a8
nabilnaja <nabilnaja9@gmail.com>
2022-03-31 23:04:30 +0200

    add leetcode 21 recursion solution

--- src/linkedList/leetcode21/Solution.java
+++ src/linkedList/leetcode21/Solution.java
@@ -23,4 +23,22 @@ public class Solution {
 
         return head.next;
     }
+
+    public ListNode mergeTwoListsRecursion(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+    }
 }
