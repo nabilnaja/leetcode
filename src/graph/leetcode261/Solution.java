f75e7a85a19358733bd27a9bf277f868e24c2ce0
nabilnaja <nabilnaja9@gmail.com>
2022-04-02 21:17:44 +0200

    add leetcode 261 solution

--- src/graph/leetcode261/Solution.java
+++ src/graph/leetcode261/Solution.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class Solution {
 
-    public boolean validTree(int n, int[][] edges) {
+    public boolean validTree_1(int n, int[][] edges) {
         List<List<Integer>> adjacencyList = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             adjacencyList.add(new ArrayList<>());
@@ -33,5 +33,35 @@ class Solution {
             }
         }
         return parents.size() == n;
+    }
+    public boolean validTree_2(int n, int[][] edges) {
+
+        if (edges.length != n - 1) return false;
+
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            adjacencyList.get(edge[0]).add(edge[1]);
+            adjacencyList.get(edge[1]).add(edge[0]);
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        Set<Integer> seen = new HashSet<>();
+        stack.add(0);
+        seen.add(0);
+
+        while (!stack.isEmpty()){
+            int node = stack.pop();
+            for(int neighbour : adjacencyList.get(node)){
+                if(seen.add(neighbour)){
+                    stack.add(neighbour);
+                }
+            }
+        }
+        return seen.size() == n;
     }
 }
