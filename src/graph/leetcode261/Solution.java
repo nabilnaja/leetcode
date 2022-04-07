58d2ff3734cf7e671cb118f235bf4ff097ae8846
nabilnaja <nabilnaja9@gmail.com>
2022-04-07 22:20:59 +0200

    add leetcode 261 solution

--- src/graph/leetcode261/Solution.java
+++ src/graph/leetcode261/Solution.java
@@ -64,4 +64,62 @@ class Solution {
         }
         return seen.size() == n;
     }
+
+    static class UnionFind {
+        private final int[] root;
+        private final int[] rank;
+
+        public UnionFind(int size) {
+            root = new int[size];
+            rank = new int[size];
+            for (int i = 0; i < size; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        public int find(int x) {
+            if (x == root[x]) {
+                return x;
+            }
+            return root[x] = find(root[x]);
+        }
+
+        public boolean union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootY == rootX) {
+                return false;
+            }
+            else {
+                if (rank[rootX] > rank[rootY]) {
+                    root[rootY] = rootX;
+                } else if (rank[rootX] < rank[rootY]) {
+                    root[rootX] = rootY;
+                } else {
+                    root[rootY] = rootX;
+                    rank[rootX] += 1;
+                }
+            }
+            return true;
+        }
+
+        public boolean connected(int x, int y) {
+            return find(x) == find(y);
+        }
+    }
+
+    public boolean validTreeUnionFind(int n, int[][] edges) {
+
+        if (edges.length != n - 1) return false;
+
+        UnionFind unionFind = new UnionFind(n);
+
+        for (int[] edge : edges) {
+            if (!unionFind.union(edge[0], edge[1])) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
