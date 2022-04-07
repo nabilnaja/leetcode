1faf952129a3400753c4c5e38f7181440f7ae70a
nabilnaja <nabilnaja9@gmail.com>
2022-04-07 22:49:13 +0200

    add leetcode 323 solution

--- src/graph/leetcode323/Solution.java
+++ src/graph/leetcode323/Solution.java
@@ -34,6 +34,52 @@ public class Solution {
             }
         });
     }
+
+    public int countComponentsUnionFind(int n, int[][] edges) {
+        UnionFind unionFind = new UnionFind(n);
+        for (int[] edge : edges) {
+            unionFind.union(edge[0], edge[1]);
+        }
+        return unionFind.count;
+    }
+    static class UnionFind {
+        private final int[] root;
+        private final int[] rank;
+        public int count;
+
+        public UnionFind(int size) {
+            root = new int[size];
+            rank = new int[size];
+            count = size;
+            for (int i = 0; i < size; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        public int find(int x) {
+            if (x == root[x]) {
+                return x;
+            }
+            return root[x] = find(root[x]);
+        }
+
+        void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                if (rank[rootX] > rank[rootY]) {
+                    root[rootY] = rootX;
+                } else if (rank[rootX] < rank[rootY]) {
+                    root[rootX] = rootY;
+                } else {
+                    root[rootY] = rootX;
+                    rank[rootX] += 1;
+                }
+                count--;
+            }
+        }
+    }
 }
 
 
